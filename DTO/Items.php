3aaa9df882da7aa3d982b2fd1b8c9aea37575d9b
Jaeuk Heo <j-heo@oz-vision.co.jp>
2018-05-17 19:18:42 +0900

    【 Items.php 】Add a variable 'Items' and set getter and setter

--- DTO/Items.php
+++ DTO/Items.php
@@ -8,5 +8,50 @@
 
 class Items
 {
+    /**
+     * @var array $items 販売の商品が入ってる
+     */
+    private $items = array(
+        'コーラ' => array(
+            'name' => 'コーラ',
+            'price' => 120,
+            'stocks' => 5
+        )
+    );
 
+    /**
+     * @param $name 商品名
+     * @return array 商品リスト
+     */
+    public function getItems()
+    {
+        return $this->items;
+    }
+
+    /**
+     * @param $name 商品名
+     * @return integer 商品値段
+     */
+    public function getItemsPrice($name)
+    {
+        return $this->items[$name]['price'];
+    }
+
+    /**
+     * @param $name 商品名
+     * @return integer 商品在庫
+     */
+    public function getItemsStocks($name)
+    {
+        return $this->items[$name]['stocks'];
+    }
+
+    /**
+     * @param $name 商品名
+     * @param $stocks 商品在庫
+     */
+    public function setItemsStocks($name, $stocks)
+    {
+        $this->items[$name]['stocks'] = $stocks;
+    }
 }
