30fa800551af5566841cb35aa77d0a970691327a
Jaeuk Heo <j-heo@oz-vision.co.jp>
2018-05-17 19:20:21 +0900

    【 Buy.php 】Add methods to initialize purchase

--- Models/Buy.php
+++ Models/Buy.php
@@ -8,5 +8,31 @@
 
 class Buy
 {
+    public function buyItems($name){
+        $items = new Items();
+        $getItemsPrice = $items->getItemsPrice($name);
+        $getBeforeStocks = $items->getItemsStocks($name);
+        $this->calculateSales($getItemsPrice);
+        $this->calculateStocks($name, $getBeforeStocks);
+        $this->calculateAmount($getItemsPrice);
+    }
 
+    public function calculateSales($price){
+        $sales = new Sales();
+        $beforeSales = $sales->getCurrentSales();
+        $currentSales = $beforeSales + $price;
+        $sales->setCurrentSales($currentSales);
+    }
+
+    public function calculateStocks($name, $stocks){
+        $items = new Items();
+        $items->setItemsStocks($name, --$stocks);
+    }
+
+    public function calculateAmount($price){
+        $bankbook = new Bankbook();
+        $getAmount = $bankbook->getCurrentAmount();
+        $currentAmount = $getAmount - $price;
+        $bankbook->setCurrentAmount($currentAmount);
+    }
 }
