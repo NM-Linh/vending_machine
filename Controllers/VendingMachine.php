7e4ddd6979ddb6164c158d90f7d5f7f1514b055f
oz-kamada <h-kamada@oz-vision.co.jp>
2018-05-21 19:07:13 +0900

    functioを作成し、必要な機能を書き出す

--- Controllers/VendingMachine.php
+++ Controllers/VendingMachine.php
@@ -1,12 +1,36 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: j-heo
  * Date: 2018/05/16
  * Time: 19:31
  */
-
 class VendingMachine
 {
+    public function display()
+    {
+
+    }
+
+    public function getAmountAndItems()
+    {
+
+    }
+
+    public function chooseAction()
+    {
+
+    }
+
+    public function powerOn()
+    {
+
+    }
+
+    public function powerOff()
+    {
+
+    }
 
 }
