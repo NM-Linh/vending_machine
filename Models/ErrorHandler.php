3a7ec7a32b4824e3139aec9d482b939c5a337141
Jaeuk Heo <j-heo@oz-vision.co.jp>
2018-05-29 23:07:32 +0900

    【Controllers/VendingMachine.php 】Fill a controller not perfect yet

---
+++ Models/ErrorHandler.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: j-heo
+ * Date: 2018/05/23
+ * Time: 18:14
+ */
+
+/**
+ * Class Error ユーザーが入力の間に起こる可能性があるエラーの処理を行う
+ */
+class Error{
+
+    public function inputNameError(){
+
+    }
+
+    public function inputMoneyError(){
+
+    }
+}
