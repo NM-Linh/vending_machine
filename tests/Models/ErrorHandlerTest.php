f943c2c7494388e5d8e68d0d374d7078137bd66d
l-nguyen <l-nguyen@oz-vision.co.jp>
2018-06-06 19:55:06 +0900

    errorHandlerのユニットテストを追加する

--- tests/Models/ErrorHandlerTest.php
+++ tests/Models/ErrorHandlerTest.php
@@ -5,7 +5,7 @@ use \PHPUnit\Framework\TestCase;
 
 class ErrorHandlerTest extends TestCase
 {
-    /** @var ErrorHandler  */
+    /** @var ErrorHandler */
     private $errorHandler;
 
     public function __construct($name = null, array $data = [], $dataName = '')
@@ -40,9 +40,42 @@ class ErrorHandlerTest extends TestCase
                 'inputValue' => '購入',
                 'expected' => '購入',
             ),
-            '数値「半角」「0↓」' => array(
+            '数値「環境依存」' => array(
+                'inputValue' => '①',
+                'expected' => false,
+            ),
+            '数値「半角」「0以下」' => array(
                 'inputValue' => -1,
                 'expected' => false,
+            ),
+            '数値「半角」「9以上」' => array(
+                'inputValue' => 10,
+                'expected' => false,
+            ),
+            '文字「想定外」' => array(
+                'inputValue' => '購入1',
+                'expected' => false,
+            ),
+        );
+    }
+
+    /**
+     * @test
+     * @dataProvider ユーザーが数値を入力したデータが正しいかテスト用テスト
+     *
+     */
+    public function ユーザーが数値を入力したデータが正しいかテスト($inputValue, $expected)
+    {
+        $result = $this->errorHandler->inputNumberError($inputValue);
+        $this->assertEquals($expected, $result);
+    }
+
+    public function ユーザーが数値を入力したデータが正しいかテスト用テスト()
+    {
+        return array(
+            '数値「半角」「0-9」' => array(
+                'inputValue' => 12,
+                'expected' => true,
             ),
         );
     }
